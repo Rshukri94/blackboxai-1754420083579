8647dcd2e9032474b7c05e0776a945787b130da2
Rshukri94 <139345957+Rshukri94@users.noreply.github.com>
2025-08-05 21:56:11 +0300

    Add booksy-clone/.next/static/chunks/src_app_professionals_page_tsx_45a68a45._.js 1754420170595

---
+++ booksy-clone/.next/static/chunks/src_app_professionals_page_tsx_45a68a45._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_components_990232f2._.js",
+  "static/chunks/node_modules_next_dist_b93b88b6._.js"
+],
+    source: "dynamic"
+});
